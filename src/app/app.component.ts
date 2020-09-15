912b30de34e5d8c46f0cdc3fc739266bccff3095
Abdenasser <nasser.elidrissi065@gmail.com>
2020-09-15 14:55:34 +0100

    displaying list of repos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { RepositoriesService } from './services/repositories.service';
+import { Repository } from './models/repository.model';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'geChallenge';
-  repos$;
+  repos$: Observable<Repository[]>;
   constructor(public repoService: RepositoriesService) {}
 
   ngOnInit() {
     this.repos$ = this.repoService.getRecentRepos();
-    this.repos$.subscribe((res) => console.log(res));
   }
 }
