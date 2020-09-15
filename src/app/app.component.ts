007dd7f6f1ad912fab2c42fcfa46885e548bb7ef
Abdenasser <nasser.elidrissi065@gmail.com>
2020-09-15 13:54:33 +0100

    inspecting results

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { RepositoriesService } from './services/repositories.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'geChallenge';
+  repos$;
+  constructor(public repoService: RepositoriesService) {}
+
+  ngOnInit() {
+    this.repos$ = this.repoService.getRecentRepos();
+    this.repos$.subscribe((res) => console.log(res));
+  }
 }
